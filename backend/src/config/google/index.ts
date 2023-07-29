d76742ab9d0f9b5fecbef11ecc33f7e461d27c3c
Alibek Bekzhanov <freed.alibek@gmail.com>
2023-07-29 19:46:14 +0600

    config fix

--- backend/src/config/google/index.ts
+++ backend/src/config/google/index.ts
@@ -1,9 +1,14 @@
 import * as process from "process";
 
 export const GoogleConfig = {
+	KEY_FILE: process.env.GOOGLE_KEY_FILE,
+
 	ROOT_MAIL: process.env.GOOGLE_ROOT_EMAIL,
+
 	CLIENT_ID: process.env.GOOGLE_ROOT_CLIENT_ID,
 	CLIENT_SECRET: process.env.GOOGLE_ROOT_CLIENT_SECRET,
 	REDIRECT_URI: process.env.GOOGLE_ROOT_REDIRECT_URI,
 	REFRESH_TOKEN: process.env.GOOGLE_ROOT_REFRESH_TOKEN,
+
+	BUCKET_NAME: process.env.GOOGLE_BUCKET_NAME,
 };
