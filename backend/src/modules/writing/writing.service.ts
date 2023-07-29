fa00cffee45a50f22e94fee38c1e20250c28fd37
Alibek Bekzhanov <freed.alibek@gmail.com>
2023-07-29 19:42:12 +0600

    writing module create getAll methods

--- backend/src/modules/writing/writing.service.ts
+++ backend/src/modules/writing/writing.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
+
+import { PrismaService } from "@modules/prisma/prisma.service";
+import { StorageService } from "@api/google/storage/storage.service";
+
+import { ObjectHelper } from "@helper/object/object.helper";
+
+import { CreateWritingDto } from "@modules/writing/dto";
+import { FilterDto } from "@root/types";
 
 @Injectable()
-export class WritingService {}
+export class WritingService {
+	constructor(
+		private readonly prismaService: PrismaService,
+		private readonly storageService: StorageService,
+		private readonly objectHelper: ObjectHelper
+	) {}
+
+	public async create(dto: CreateWritingDto, file?: Express.Multer.File) {
+		dto = <CreateWritingDto>this.objectHelper.parseNumbers(dto);
+
+		if (dto.taskType === 1 && file) {
+			await this.storageService.upload(file, "writing").then((response) => {
+				dto.path = response;
+			});
+		}
+
+		return await this.prismaService.writing
+			.create({
+				data: {
+					taskType: dto.taskType,
+					thesis: dto.thesis,
+					path: dto.path,
+				},
+			})
+			.then((writing) => {
+				return writing;
+			})
+			.catch((error) => {
+				console.log(error);
+				throw new InternalServerErrorException(error);
+			});
+	}
+
+	public async getAll(dto: FilterDto) {
+		dto = this.objectHelper.removeNulls(dto);
+		dto = this.objectHelper.parseNumbers(dto);
+
+		return await this.prismaService.writing
+			.findMany({
+				...dto,
+			})
+			.then((writings) => {
+				return writings;
+			})
+			.catch((error) => {
+				throw new InternalServerErrorException(error);
+			});
+	}
+}
