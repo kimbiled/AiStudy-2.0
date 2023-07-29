817235b79433cd9b3ed7de5233e2d1abb226d328
Alibek Bekzhanov <freed.alibek@gmail.com>
2023-07-29 16:46:48 +0600

    types for env

--- backend/src/env.d.ts
+++ backend/src/env.d.ts
@@ -1,7 +1,13 @@
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
-			BACKEND_PORT: 8000;
+			readonly BACKEND_PORT: 8000;
+
+			readonly GOOGLE_ROOT_EMAIL: string;
+			readonly GOOGLE_ROOT_CLIENT_ID: string;
+			readonly GOOGLE_ROOT_CLIENT_SECRET: string;
+			readonly GOOGLE_ROOT_REDIRECT_URI: string;
+			readonly GOOGLE_ROOT_REFRESH_TOKEN: string;
 		}
 	}
 }
