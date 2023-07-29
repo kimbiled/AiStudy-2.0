b7cc5f0daa9e1eb43387c50207334d057cfc1499
Alibek Bekzhanov <freed.alibek@gmail.com>
2023-07-29 19:41:14 +0600

    adding env vars

--- backend/src/env.d.ts
+++ backend/src/env.d.ts
@@ -3,11 +3,13 @@ declare global {
 		interface ProcessEnv {
 			readonly BACKEND_PORT: 8000;
 
+			readonly GOOGLE_KEY_FILE: string;
 			readonly GOOGLE_ROOT_EMAIL: string;
 			readonly GOOGLE_ROOT_CLIENT_ID: string;
 			readonly GOOGLE_ROOT_CLIENT_SECRET: string;
 			readonly GOOGLE_ROOT_REDIRECT_URI: string;
 			readonly GOOGLE_ROOT_REFRESH_TOKEN: string;
+			readonly GOOGLE_BUCKET_NAME: string;
 		}
 	}
 }
