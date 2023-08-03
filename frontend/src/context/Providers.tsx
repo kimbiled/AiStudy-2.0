91adec58fd8f04ec53f1e5d34264aae746f5512f
Alibek Bekzhanov <freed.alibek@gmail.com>
2023-08-03 20:00:42 +0600

    writing context

--- frontend/src/context/Providers.tsx
+++ frontend/src/context/Providers.tsx
@@ -1,14 +1,17 @@
 import { ReactNode } from "react";
 
-import { AuthProvider } from "./auth/useAuth";
-import { UserProvider } from "./user/useUser.tsx";
 import { CustomCookieProvider } from "./CustomCookies/useCustomCookies.tsx";
+import { UserProvider } from "./user/useUser.tsx";
+import { AuthProvider } from "./auth/useAuth";
+import { WritingProvider } from "./writing/useWriting.tsx";
 
 export default function Providers({ children }: { children: ReactNode }) {
 	return (
 		<CustomCookieProvider>
 			<UserProvider>
-				<AuthProvider>{children}</AuthProvider>
+				<AuthProvider>
+					<WritingProvider>{children}</WritingProvider>
+				</AuthProvider>
 			</UserProvider>
 		</CustomCookieProvider>
 	);
